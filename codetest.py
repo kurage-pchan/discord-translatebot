f3779927c78ccf68e197eaba925368066663cdcb
kir1n <68494316+kurage-pchan@users.noreply.github.com>
2023-10-12 21:19:21 +0900

    Update codetest.py

--- codetest.py
+++ codetest.py
@@ -1,12 +1,6 @@
 import discord
 
-now
-
-Update discordbot.py
 Token = "ここにトークンを入力"
-2 minutes ago
-
-Add files via upload
 
 Intents = discord.Intents.default()
 Intents.message_content = True
@@ -25,20 +19,10 @@ async def on_message(message):
         return
 
     if message.content == '/ohayou':
-now
-
-Update discordbot.py
         await message.channel.send("おはBot")
-2 minutes ago
-
-Add files via upload
-
+        
     if message.content.startswith('おやすみ'):
         await message.channel.send('おやすみ！また明日！')
         await client.close()
 
-
-now
-
-Update discordbot.py
 client.run(Token)
